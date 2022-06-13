35f5edaf053bba660310af456312ea0ecad7acb4
Esteban-DdS <ztheeban205@gmail.com>
2022-06-13 14:52:07 -0500

    agregar contenido, validar las imagenes y devolver valores

--- controllers/articleController.php
+++ controllers/articleController.php
@@ -24,12 +24,14 @@ class ArticleController {
     public function addArticle() {
         Utils::isAdmin();
         if (isset($_POST)){
-            
+            $result = true;
+
             $title = isset($_POST['title']) ? trim($_POST['title']) : null;
             $email = isset($_POST['email']) ? trim($_POST['email']) : null;
             $content = isset($_POST['content']) ? trim($_POST['content']) : null;
+            $email_validate = filter_var($email, FILTER_VALIDATE_EMAIL);
 
-            if ($title && $email && $content) {
+            if ($title && $email_validate && $content) {
 
                 $article = new ArticleModel();
                 
@@ -39,33 +41,47 @@ class ArticleController {
 
 
                 // Validamos si nos han enviado la imagén
-                if (isset($_FILES['image'])) {
+                if (isset($_FILES['image']) && $_FILES['image']['name']) {
                     $file = $_FILES['image'];
                     $filename = $file['name'];
                     $mimetype = $file['type'];
-
+                    
                     if($mimetype == "image/jpg" || $mimetype == 'image/jpeg' || $mimetype == 'image/png'){
-
+                        
                         if(!is_dir('uploads/images')){
                             mkdir('uploads/images', 0777, true);
                         }
-
+                        
                         $article->setPicture($filename);
                         move_uploaded_file($file['tmp_name'], 'uploads/images/'.$filename);
                     }
+                } else {
+                    $result = false;
+                }
+                
+                if ($result) {
+                    // Validamos si lo que se quiere es editar el articulo
+                    if (isset($_GET['id'])) {
+                        $id = $_GET['id'];
+                        $article->setId($id);
+                        
+                        $update= $article->editArticle();
+                    } else {
+                        $article = $article->addArticle();
+                    }
                 }
 
-                if (isset($_GET['id'])) {
-                    $id = $_GET['id'];
-                    $article->setId($id);
-                    
-                    $update= $article->editArticle();
-                } else {
-                    $article = $article->addArticle();
-                }
+            } else {
+                $result = array (
+                    'addArticle' => false,
+                    'msg_error' => "Llena todos los campos correctamente"
+                );
             }
+
+            echo json_encode($result);
         }
-        return header('Location: '.BASE_URL);   
+        
+        return header('Location: '.BASE_URL);
     }
 
     public function deleteArticle() {
@@ -75,23 +91,22 @@ class ArticleController {
             $id = $_GET['id'];
             $article = new ArticleModel();
             $article->setId($id);
-            $article->deleteArticle();
+            $delete = $article->deleteArticle();
 
-            if ($article) {
+            if ($delete) {
                 $result = array(
-                    'delete' => $article,
-                    'msg_delete' => 'Eliminado éxitosamente'
+                    'delete' => $delete,
+                    'msg_delete' => 'Artículo eliminado éxitosamente'
                 );
             } else {
                 $result = array(
-                    'delele' => $article,
+                    'delete' => $delete,
                     'msg_delete' => 'Ha ocurrido un error'
                 );
             }
         }
 
-        return $result;
-        // debe retornar a una página
+        echo json_encode($result);
     }
 
 }
