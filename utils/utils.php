a46de4ee3d64a2aeb161649362d7f96bc12b840c
Esteban-DdS <ztheeban205@gmail.com>
2022-06-14 19:09:04 -0500

    funciones para validar valores implementadas

--- utils/utils.php
+++ utils/utils.php
@@ -14,7 +14,50 @@ class Utils {
         }
         return true;
     }
-    
+
+    /**
+     * validar que las imagenes sean correctas
+     * @return bool
+     */
+    public static function validateImage($class, $files) {
+        $file = $files['image'];
+        $filename = $file['name'];
+        $mimetype = $file['type'];
+        
+        if($mimetype == "image/jpg" || $mimetype == 'image/jpeg' || $mimetype == 'image/png'){
+            
+            if(!is_dir('uploads/images')){
+                mkdir('uploads/images', 0777, true);
+            }
+            
+            $class->setPicture($filename);
+            move_uploaded_file($file['tmp_name'], 'uploads/images/'.$filename);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * validar los campos enviados
+     * @return objeto o false
+     */
+    public static function validateInputs($post) {
+        $title = isset($post['title']) ? trim($post['title']) : null;
+        $email = isset($post['email']) ? trim($post['email']) : null;
+        $content = isset($post['content']) ? trim($post['content']) : null;
+        $email_validate = filter_var($email, FILTER_VALIDATE_EMAIL);
+
+        if ($title && $email_validate && $content) {
+            $article = new ArticleModel();
+            
+            $article->setTitle($title);
+            $article->setEmailUser($email_validate);
+            $article->setContent($content);
+            return $article;
+        }
+
+        return false;
+    }
 }
 
 ?>
