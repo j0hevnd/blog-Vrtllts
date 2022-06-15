411f38930a37aa5ad55997ce4977b72e329812b8
Esteban-DdS <ztheeban205@gmail.com>
2022-06-14 19:11:27 -0500

    mejoras a funcionalidades de agregar y editar, recaptcha implementado

--- controllers/articleController.php
+++ controllers/articleController.php
@@ -3,6 +3,7 @@
 require_once 'models/article.php';
 
 class ArticleController {
+
     /**
      * vista de inicio de la pagina
      */
@@ -15,6 +16,7 @@ class ArticleController {
         
     }
     
+
     public function find() {
         Utils::isAdmin();
         $url = explode('/', $_GET['url']);
@@ -31,66 +33,69 @@ class ArticleController {
         echo json_encode($response);
     }
 
+
     public function edit() {
         Utils::isAdmin();
         if(isset($_POST)) {
-            $result = false;
+            // funcionalidad para recaptcha
+            require_once "utils/recaptchalib.php";   
+            $secret = "6LcLyG4gAAAAAFF89il9ho3gG6e2lCvV-QwbB62w";
+            $response = null;
+            // Verificamos la clave secreta
+            $reCaptcha = new ReCaptcha($secret);
+            if ($_POST["g-recaptcha-response"]) {
+                $response = $reCaptcha->verifyResponse(
+                    $_SERVER["REMOTE_ADDR"],
+                    $_POST["g-recaptcha-response"]
+                );
+            }
 
-            $title = isset($_POST['title']) ? trim($_POST['title']) : null;
-            $email = isset($_POST['email']) ? trim($_POST['email']) : null;
-            $content = isset($_POST['content']) ? trim($_POST['content']) : null;
-            $email_validate = filter_var($email, FILTER_VALIDATE_EMAIL);
+            if ($response != null && $response->success) {
+                // Añade aquí el código que desees en el caso de que la validación sea correcta
 
-            if ($title && $email_validate && $content) {
-                $result = true;
-                $article = new ArticleModel();
-                
-                $article->setTitle($title);
-                $article->setEmailUser($email_validate);
-                $article->setContent($content);
-                
-                
-                // Validamos si nos han enviado la imagén
-                if (isset($_FILES['image']) && $_FILES['image']['name']) {
-                    $file = $_FILES['image'];
-                    $filename = $file['name'];
-                    $mimetype = $file['type'];
-                    
-                    if($mimetype == "image/jpg" || $mimetype == 'image/jpeg' || $mimetype == 'image/png'){
-                        
-                        if(!is_dir('uploads/images')){
-                            mkdir('uploads/images', 0777, true);
-                        }
-                        
-                        $article->setPicture($filename);
-                        move_uploaded_file($file['tmp_name'], 'uploads/images/'.$filename);
+                // Valida los datos que nos llegan por post
+                if ($article = Utils::validateInputs($_POST)) {
+
+                    // Validamos si nos han enviado la imagén
+                    if (isset($_FILES['image']) && $_FILES['image']['name']) {
+                        Utils::validateImage($article, $_FILES);
                     }
-                }
 
-                
-                // obtenemos el id de la url
-                $url = explode('/', $_GET['url']);
-                $id = (int) end($url);
-                if(isset($id)) {                    
-                    $article->setId($id);
-                    $result = $article->editArticle();
+                    // obtenemos el id de la url
+                    $url = explode('/', $_GET['url']);
+                    $id = (int) end($url);
+                    if(isset($id)) {                    
+                        $article->setId($id);
+                        // edita la entrada
+                        $result = $article->editArticle();
+                    }
+                    
+                    if ($result) {
+                        $result = array (
+                            'editEntry' => $result,
+                            'msg' => "Actualizado éxitosamente"
+                        );
+                    } else {
+                        $result = array (
+                            'editEntry' => $result,
+                            'msg' => "Error al actualizar"
+                        );
+                    }
+
                 } else {
-                    $result = false;
-                }
-
-                if (!$result) {
                     $result = array (
-                        'editEntry' => $result,
+                        'editEntry' => $article,
                         'msg' => "Llena todos los campos correctamente"
                     );
-                } else {
-                    $result = array (
-                        'editEntry' => $result,
-                        'msg' => "Actualizado éxitosamente"
-                    );
                 }
 
-            } // validates
+            } else {
+                // Añade aquí el código que desees en el caso de que la validación no sea correcta o muestra
+                $result = array (
+                    'editEntry' => $response,
+                    'msg' => "No válido"
+                );
+            }
 
             echo json_encode($result);
         } // POST
@@ -98,64 +103,61 @@ class ArticleController {
 
     public function addArticle() {
         if (isset($_POST)){
-            $result = false;
-
-            $title = isset($_POST['title']) ? trim($_POST['title']) : null;
-            $email = isset($_POST['email']) ? trim($_POST['email']) : null;
-            $content = isset($_POST['content']) ? trim($_POST['content']) : null;
-            $email_validate = filter_var($email, FILTER_VALIDATE_EMAIL);
-
-            if ($title && $email_validate && $content) {
-                $result = true;
-                $article = new ArticleModel();
-                
-                $article->setTitle($_POST['title']);
-                $article->setEmailUser($_POST['email']);
-                $article->setContent($_POST['content']);
-                
-                
-                // Validamos si nos han enviado la imagén
-                if (isset($_FILES['image']) && $_FILES['image']['name']) {
-                    $file = $_FILES['image'];
-                    $filename = $file['name'];
-                    $mimetype = $file['type'];
-                    
-                    if($mimetype == "image/jpg" || $mimetype == 'image/jpeg' || $mimetype == 'image/png'){
-                        
-                        if(!is_dir('uploads/images')){
-                            mkdir('uploads/images', 0777, true);
-                        }
-                        
-                        $article->setPicture($filename);
-                        move_uploaded_file($file['tmp_name'], 'uploads/images/'.$filename);
-                    }
-                } else {
-                    $result = false;
-                }
-                
-                if ($result) {
-                    $result = $article->addArticle();
-                }
-
-            }
-
-            if (!$result) {
-                $result = array (
-                    'article' => $result,
-                    'msg' => "Llena todos los campos correctamente"
-                );
-            } else {
-                $result = array (
-                    'article' => $result,
-                    'msg' => "Agregado éxitosamente"
+            // funcionalidad para recaptcha
+            require_once "utils/recaptchalib.php";   
+            $secret = "6LcLyG4gAAAAAFF89il9ho3gG6e2lCvV-QwbB62w";
+            $response = null;
+            // Verificamos la clave secreta
+            $reCaptcha = new ReCaptcha($secret);
+            if ($_POST["g-recaptcha-response"]) {
+                $response = $reCaptcha->verifyResponse(
+                $_SERVER["REMOTE_ADDR"],
+                $_POST["g-recaptcha-response"]
                 );
             }
             
+            if ($response != null && $response->success) {
+                // Añade aquí el código que desees en el caso de que la validación sea correcta
+                
+                // valida los campos que nos llegan por POST
+                $class_result = Utils::validateInputs($_POST);
+
+                // Validamos si nos han enviado la imagén
+                if (isset($_FILES['image']) && $_FILES['image']['name']) {
+                    $result = Utils::validateImage($class_result['class'], $_FILES);
+                }
+
+                // guardamos la entrada en la db, en caso contrario retorna false
+                if ($result) {
+                    $result = $class_result['class']->addArticle();
+                }            
+
+                if (!$result) {
+                    $result = array (
+                        'article' => $result,
+                        'msg' => "Llena todos los campos correctamente"
+                    );
+                } else {
+                    // devuelve el objeto creado para ser manupulado en el JS
+                    $result = array (
+                        'article' => $result,
+                        'msg' => "Agregado éxitosamente"
+                    );
+                }
+
+            } else {
+               // Añade aquí el código que desees en el caso de que la validación no sea correcta o muestra
+                $result = array (
+                    'article' => $response,
+                    'msg' => "No válido"
+                );
+            }
+
             echo json_encode($result);
 
         } // POST        
-        // return header('Location: '.BASE_URL);
     }
+
 
     public function deleteArticle() {
         Utils::isAdmin();
